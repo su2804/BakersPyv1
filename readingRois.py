36f1d57ec379a64da7104eb12a2161db720bebc2
su2804 <60634393+su2804@users.noreply.github.com>
2021-04-24 16:55:32 -0700

    Update readingRois.py

--- readingRois.py
+++ readingRois.py
@@ -1,6 +1,6 @@
 
 #Read roi files from ImageJ
-#sys.path.insert(0,"/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/read_roi/_read_roi.py")
+
 from read_roi import read_roi_file
 from read_roi import read_roi_zip
 roi = read_roi_zip('/Users/Saransh/Desktop/formichaelray/formichael/RoiSet.zip')
@@ -8,4 +8,4 @@ roi = read_roi_zip('/Users/Saransh/Desktop/formichaelray/formichael/RoiSet.zip')
 a=list(roi.items())
 print(a[0][1])
 
-print(a[0][1]['left'])
+print(a[0][1]['height'])
