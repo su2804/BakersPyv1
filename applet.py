d9c12537e9fd96f884e426328496a4c2ffcfc45a
Saransh Umale <Saransh@junlab09.junlab.ucsd.edu>
2022-01-23 01:29:57 -0800

    Add existing file

--- applet.py
+++ applet.py
@@ -104,7 +104,7 @@ class UI(QDialog):
                         if maskindex<11:
 
                             #extract roi's
-                            roi_tuples= extract_roi_coordinates(roi_path+'fov'+str(maskindex)+'_cyto.zip');
+                            roi_tuples= extract_roi_coordinates(roi_path+'fov'+str(maskindex)+'.zip');
                             maskimage=cv2.imread(mask_path+'mask000'+str(maskindex-1)+'.tif',cv2.IMREAD_UNCHANGED)
                             maskarray=np.array(maskimage)
                             maskarray.astype(int)
@@ -190,7 +190,7 @@ class UI(QDialog):
                         elif maskindex>10 & maskindex<101:
 
                             #extract roi's
-                            roi_tuples= extract_roi_coordinates(roi_path+'fov'+str(maskindex)+'_cyto.zip');
+                            roi_tuples= extract_roi_coordinates(roi_path+'fov'+str(maskindex)+'.zip');
                             maskimage=cv2.imread(mask_path+'mask00'+str(maskindex-1)+'.tif',cv2.IMREAD_UNCHANGED)
                             maskarray=np.array(maskimage)
                             maskarray.astype(int)
