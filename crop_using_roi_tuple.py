3f2a3cb66a17213a3b8ecff0d2df59a978955d42
su2804 <60634393+su2804@users.noreply.github.com>
2022-01-22 17:53:59 -0800

    Delete crop_using_roi_tuple.py

--- crop_using_roi_tuple.py
+++
@@ -1,9 +0,0 @@
-
-#uses roi coordinates tuple to crop a part of the input image. Will be used to crop out individual cells from the image.
-from PIL import Image
-def crop_using_roi_tuple(coordinates_tuple,image):
-    im=Image.open(image);
-    im1 = im.crop(coordinates_tuple);
-    return im1
-
-    
